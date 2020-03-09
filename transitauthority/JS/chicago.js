97f44538f929a16f91edf0e94d624f23a52e975d
Ramona Saintandre <codinggrandma@gmail.com>
2020-03-08 21:07:32 -0400

    finished getStations funcitions, need to document

--- transitauthority/JS/chicago.js
+++ transitauthority/JS/chicago.js
@@ -1,21 +1,24 @@
-let url = '';  //holds the url created to query the API
-let out = '';  // output function 
-let station = '';  // station that is currently selected function
+let url = ''; //holds the url created to query the API
+let out = ''; // output function 
+let station = ''; // station that is currently selected function
 
-window.onload = function(){  // onload event waits for everthing to load before excuting the getStations functions 
-getStations();
+window.onload = function () { // onload event waits for everthing to load before excuting the getStations functions 
+  getStations();
 }
 
-getStations = function() {
+getStations = function () {
   let out = "<select  id=<'stationSelect'>";
-  for (var x = 0; x < stations.length; x++){
+  for (var x = 0; x < stations.length; x++) {
     out += "<option value = ' " + stations[x].number + "'>";
     out += stations[x].name;
     out += "</option>";
   }
   out += "</select>";
   document.getElementById('stationSched').innerHTML = out;
-document.getElementById('stationSelect').addEventListener('change', function (){
-
-})
+  document.getElementById('stationSelect').addEventListener('change', function () {
+    station = document.getElementById('stationSelect').value;
+    url += "access.php/?station=" + station;
+    getTrains(stations);
+    url = "";
+  })
 }
