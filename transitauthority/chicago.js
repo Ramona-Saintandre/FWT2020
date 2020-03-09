0ac68ff11d8c4e6e8fa48c66457c65341d407b25
Ramona Saintandre <codinggrandma@gmail.com>
2020-03-08 20:31:24 -0400

    started setting up chicago.js

---
+++ transitauthority/chicago.js
@@ -0,0 +1,13 @@
+let url = '';
+let out = '';
+let station = '';
+
+window.onload = function(){
+  let out = "<select  id=<'stationSelect'>";
+  for (var x = 0; x < stations.length; x++){
+    out += "<option value = ' " + stations[x].number + "'>";
+    out += stations[x].name;
+    out += "</option>";
+  }
+  out += "</select>";
+}
