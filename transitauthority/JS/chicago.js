3cc880acc68cc05de29dcffa3de6687e317fd319
Ramona Saintandre <codinggrandma@gmail.com>
2020-03-08 22:39:51 -0400

    setting up getTrainInfo function/loop

--- transitauthority/JS/chicago.js
+++ transitauthority/JS/chicago.js
@@ -37,8 +37,8 @@ getStations = function () {
     console.log(out);
     document.getElementById('trainInfo').innerHTML = out;  //display the data  
     
-    // const trainArray = data.ctatt.eta;
-    
+   
+
     console.log(trainArray);
   }).catch(error => console.log(error));  //catch's errors that come along the way 
 }
@@ -46,6 +46,20 @@ getStations = function () {
 // Train information to be displayed 
 
   getTrainInfo = function(data) {
-document.getElementById('trainInfo').innerHTML = "";
+document.getElementById('trainInfo').innerHTML = out;
 out = "";
+const trainArray = data.ctatt.eta;
+console.log(trainArray);
+const station = trainArray[0].staNm;
+out += "<h1>" + station + "</h1>";
+
+for (var x=0; x <trainArray.length; x++){
+  const destination = trainArray[x].destNm;
+  let arrivalTime = trainArray[x].arrT;
+  arrivalTime = moment.tz(arrivalTime, "America/Chicago");
+  arrivalTime = moment(arrivalTime).fromNow();
+  const rt = trainArray[x].rt;
+  const description = trainArray[x].stpDe;
+  const approaching = trainArray[x].isApp;
+}
   }
